05c39b204b83e1c69e7fbeae934c66b59aa1ae66
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2022-03-03 08:46:39 +0100

    added missing lib

--- Assets/MirageSDK/Plugins/WalletConnectSharp.Unity/WalletConnect.cs
+++ Assets/MirageSDK/Plugins/WalletConnectSharp.Unity/WalletConnect.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
